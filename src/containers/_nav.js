ae66eb92ac687e8a20996532ac75bfa9575b9596
Bitdemo <saini1512810056@gmail.com>
2021-05-29 08:55:40 +0530

    nishant

--- src/containers/_nav.js
+++ src/containers/_nav.js
@@ -4,8 +4,8 @@ import CIcon from '@coreui/icons-react'
 const _nav =  [
   {
     _tag: 'CSidebarNavItem',
-    name: 'Dashboard',
-    to: '/dashboard',
+    name: 'Chatapp',
+    to: '/chatapp',
     icon: <CIcon name="cil-speedometer" customClasses="c-sidebar-nav-icon"/>,
     badge: {
       color: 'info',
